c0eca3002a99bf9e5b528994d6bdeb36bec818a5
osa <osa@osa-p.net>
2020-08-20 18:49:37 +0900

    サイトロゴが表示されなくなっていたので調整

--- distsns2notestock.meta.js
+++ distsns2notestock.meta.js
@@ -2,11 +2,13 @@
 // @name         distsns to notestock
 // @namespace    osa-p.net
 // @author       osa <osa@osa-p.net>
-// @version      1.1.0
+// @version      1.1.1
 // @homepageURL  https://notestock.osa-p.net
 // @grant           GM_xmlhttpRequest
 // @grant           GM.xmlHttpRequest
 // @match        https://*/@*
 // @match        https://*/notes/*
+// @updateURL    https://github.com/osapon/distsns2notestock/raw/master/distsns2notestock.user.js
 // @downloadURL  https://github.com/osapon/distsns2notestock/raw/master/distsns2notestock.user.js
+// @supportURL   https://github.com/osapon/distsns2notestock
 // ==/UserScript==
